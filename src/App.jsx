a89725c1de7c10d2659c7a9b79bb1ef14427431d
Het Mardiya <hetmardiya9@gmail.com>
2024-09-26 18:45:33 +0530

    add some animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import { CopyIcon } from '@chakra-ui/icons'
+import { RiFindReplaceLine } from "react-icons/ri";
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div className='w-full min-w-[100%] h-screen bg-green-400'>{/* this div is for animation */}
+      <div className='w-full min-w-[100%] h-screen animate-color-change'>{/* this div is for animation */}
         <div className='w-full h-screen flex justify-center items-center'> {/* this div is for box */}
           <div className='w-auto max-w-[80%] h-auto bg-gray-300 flex flex-col p-10 rounded-md'>
             <p className='font-sofadi font-semibold text-3xl leading-8 tracking-wider'>helllo welcom tho the shoqSure! Here are three additional offers for your E-Carworkshop:
@@ -13,8 +14,8 @@ function App() {
 Limited Time Battery Check Offer
  </p>
             <div className='flex mt-3 justify-center items-center space-x-4'>
-              <button className='bg-indigo-300 px-4 py-2 border-2 font-medium text-[17px] rounded-lg hover:bg-indigo-400 hover:border-2 hover:border-indigo-700 hover:scale-105 transition duration-300 delay-150 ease-in-out will-change-transform'>Generate Quote</button>
-              <button className='bg-indigo-300 px-4 py-2 border-2 font-medium text-[17px] rounded-lg hover:bg-indigo-400 hover:border-2 hover:border-indigo-700 hover:scale-105 transition duration-300 delay-150 ease-in-out will-change-transform'>Copy</button>
+              <button className='bg-indigo-300 px-4 py-2 border-2 font-medium text-[17px] rounded-lg hover:bg-indigo-400 hover:border-2 hover:border-indigo-700 hover:scale-105 transition duration-300 delay-150 ease-in-out will-change-transform flex justify-center items-center space-x-1'> <RiFindReplaceLine/><span className=''>Generate Quote</span></button>
+              <button className='bg-indigo-300 px-4 py-2 border-2 font-medium text-[17px] rounded-lg hover:bg-indigo-400 hover:border-2 hover:border-indigo-700 hover:scale-105 transition duration-300 delay-150 ease-in-out will-change-transform flex justify-center items-center space-x-1'> <CopyIcon/> <span>Copy</span> </button>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,19 @@ export default {
     extend: {
       fontFamily:{
         sofadi:['Sofadi One' , 'system-ui']
-      }
+      },
+      animation: {
+        'color-change': 'colorChange 10s ease-in-out infinite alternate', // Custom animation name, duration, and infinite loop
+      },
+      keyframes: {
+        colorChange: {
+          '0%': { backgroundColor: '#9b5de5' },
+          '25%':{backgroundColor : '#f15bb5'},
+          '50%': { backgroundColor: '#fee440' },
+          '75%':{backgroundColor:'#00bbf9'},
+          '100%':{backgroundColor:'#00f5d4'}
+        },
+      },
     },
   },
   plugins: [],
